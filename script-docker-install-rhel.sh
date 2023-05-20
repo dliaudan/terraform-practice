da68a698a3f7ffeae9f46bb9d3fa9ecb0f555d89
dliaudan <130764536+dliaudan@users.noreply.github.com>
2023-05-20 16:20:11 +0300

    Changing main file and bash script
    1. Renaming file from ec2.tf to main.tf
    2. Correcting bash script for installing docker: add attribute for yum install (-y)

--- script-docker-install-rhel.sh
+++ script-docker-install-rhel.sh
@@ -5,6 +5,6 @@
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo systemctl start docker
 sudo docker run hello-world

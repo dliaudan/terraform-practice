170b81cdd860a4ff9ed692114bf9c33b7f5f0925
dliaudan <130764536+dliaudan@users.noreply.github.com>
2023-05-20 14:59:10 +0300

    Create script-docker-install-rhel.sh

---
+++ script-docker-install-rhel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#This script install a docker compose.
+#Note: here used centos repo cause for latast version of RHEL (currently 9) doesn't exist
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl start docker
+sudo docker run hello-world

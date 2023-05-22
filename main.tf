7cf282b795262660dfc19950a17da949d31febed
dliaudan <130764536+dliaudan@users.noreply.github.com>
2023-05-22 18:53:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region     = "eu-north-1"
   profile    = "admin"
 }
-
+#how to change profile for different users?
 
 #create vpc for ec2
 resource "aws_vpc" "main_for_terraform" {
@@ -68,7 +68,7 @@ resource "aws_security_group" "terraform_default_SG" {
   vpc_id      = aws_vpc.main_for_terraform.id
 #each ingress means one ingress rule
   ingress {
-    description      = "HTTPS from VPC"
+    description      = "HTTPS from EC2 instance"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
@@ -76,7 +76,7 @@ resource "aws_security_group" "terraform_default_SG" {
   }
 
   ingress {
-    description      = "SSH from VPC"
+    description      = "SSH from EC2 instance"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
@@ -84,7 +84,7 @@ resource "aws_security_group" "terraform_default_SG" {
   }
 
   ingress {
-    description      = "HTTP from VPC"
+    description      = "HTTP from EC2 instance"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
@@ -139,4 +139,3 @@ resource "aws_eip" "elasticip" {
 output "EIP" {
     value = aws_eip.elasticip.public_ip
 }
-#FINISH

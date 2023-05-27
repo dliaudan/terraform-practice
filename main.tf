c84c2abd44d05d5abc5515297ec85b21f4e221d5
dliaudan <130764536+dliaudan@users.noreply.github.com>
2023-05-27 12:14:56 +0300

    Changed name of resources according to standards
    All resources' names were changed to "this" or which not has uppercase and have only underscore

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 
 #create vpc for ec2
-resource "aws_vpc" "main_for_terraform" {
+resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
 
@@ -26,8 +26,8 @@ resource "aws_vpc" "main_for_terraform" {
 }
 
 #create subnet for vpc
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main_for_terraform.id
+resource "aws_subnet" "this" {
+  vpc_id     = aws_vpc.this.id
   cidr_block = "10.0.11.0/24"
   availability_zone = "eu-north-1a"
 
@@ -37,8 +37,8 @@ resource "aws_subnet" "main" {
 }
 
 #create gateway for vpc
-resource "aws_internet_gateway" "GW-for-TF" {
-  vpc_id = aws_vpc.main_for_terraform.id
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
 
   tags = {
     Name = "terraform"
@@ -46,27 +46,27 @@ resource "aws_internet_gateway" "GW-for-TF" {
 }
 
 #create routing table
-resource "aws_route_table" "route-table-aws" {
-  vpc_id = aws_vpc.main_for_terraform.id
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.GW-for-TF.id
+    gateway_id = aws_internet_gateway.this.id
   }
 #IMPORTANT: local routes created by default. No need to configure here.
 }
 
 #creating route table association
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.route-table-aws.id
+resource "aws_route_table_association" "this" {
+  subnet_id      = aws_subnet.this.id
+  route_table_id = aws_route_table.this.id
 }
 
 #creating security group
-resource "aws_security_group" "terraform_default_SG" {
+resource "aws_security_group" "this" {
   name        = "terraform_security_group"
   description = "Allow 22, 80, 443 ports"
-  vpc_id      = aws_vpc.main_for_terraform.id
+  vpc_id      = aws_vpc.this.id
 #each ingress means one ingress rule
   ingress {
     description      = "HTTPS from EC2 instance"
@@ -104,11 +104,12 @@ resource "aws_security_group" "terraform_default_SG" {
   }
 }
 
-resource "aws_instance" "web-server-test" {
+#creating EC2 instance 
+resource "aws_instance" "web_server_test" {
   ami                    = "ami-0a6351192ce04d50c"
   instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.terraform_default_SG.id]
+  subnet_id              = aws_subnet.this.id
+  vpc_security_group_ids = [aws_security_group.this.id]
   key_name               = "keypairssh" 
   user_data              = file("script-docker-install-rhel.sh")
   
@@ -118,25 +119,27 @@ resource "aws_instance" "web-server-test" {
 }
 
 #creating EBS resources (more RAM) and attach it to the EC2 instance
-resource "aws_ebs_volume" "aws_ebs_volume_terraform" {
+resource "aws_ebs_volume" "this" {
   availability_zone     = "eu-north-1a"
   size                  = 8 #8 GiB volume of EBS
   type                  = "gp2" #general purpose
 
 }
 
-resource "aws_volume_attachment" "attachment_for_ebs_terraform" {
+#creating attachment for connecting EC2 instance with EBS volume
+resource "aws_volume_attachment" "this" {
   device_name           = "/dev/xvdh"
-  volume_id             = "${aws_ebs_volume.aws_ebs_volume_terraform.id}"
-  instance_id           = "${aws_instance.web-server-test.id}" 
+  volume_id             = "${aws_ebs_volume.this.id}"
+  instance_id           = "${aws_instance.web_server_test.id}" 
 }
 
 #create elastic IP for EC2 instance
-resource "aws_eip" "elasticip" {
-  instance = aws_instance.web-server-test.id
+resource "aws_eip" "this" {
+  instance = aws_instance.web_server_test.id
 }
 
 #showing elastic ip for me in cli
-output "EIP" {
-    value = aws_eip.elasticip.public_ip
+output "elastic_ip_output" {
+    description = "Gives output of elastic IP for EC2 instance"
+    value = aws_eip.this.public_ip
 }

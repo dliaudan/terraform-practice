2cbc5dbde02cc123a1a68534581236b398088c5b
dliaudan <130764536+dliaudan@users.noreply.github.com>
2023-05-28 01:33:15 +0300

    Create tfvars file
    This file include "custom" data for vpc, ec2 instance etc.
    
    In this commit, *tfvars file is not ignored, but on the next commit they will do

---
+++ terraform.tfvars
@@ -0,0 +1,24 @@
+# here you can enter non default variables
+
+#====================
+# Variables for VPC #
+#====================
+
+cidr = "10.0.0.0/16"
+
+cidr_block = "10.0.11.0/24"
+
+azs = "eu-north-1a"
+
+
+#============================
+# Variables for EC2 and EBS #
+#============================
+
+ami_type = "ami-0a6351192ce04d50c"
+
+instance_type = "t3.micro"
+
+key_pair_ssh = "keypairssh"
+
+ebs_size = 8
--- variables.tf
+++ variables.tf
@@ -1,24 +1,26 @@
 #this file for future variables during terraform restructuring
 
+#note: all default variables you can specify on your own purposes. To edit variables see variables.tfvars file
+
 #====================
 # Variables for VPC #
 #====================
 variable "cidr" {
-  description = "The CIDR block for the VPC. Default value is a valid CIDR"
+  description = "The CIDR block for the VPC. By default, you need to specify it in your variables.tfvars"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = ""
 }
 
 variable "cidr_block" {
-  description = "The CIDR block for the subnet. Default value is a valid CIDR"
+  description = "The CIDR block for the subnet. By default, you need to specify it in your variables.tfvars"
   type        = string
-  default     = "10.0.11.0/24"
+  default     = ""
 }
 
 variable "azs" {
-  description = "The Availability zone for VPC. Currently, here is only 1 AZ"
+  description = "The Availability zone for VPC. By default, you need to specify it in your variables.tfvars"
   type        = string
-  default     = "eu-north-1a"
+  default     = ""
 }
 
 #============================
@@ -26,25 +28,25 @@ variable "azs" {
 #============================
 
 variable "ami_type" {
-  description = "The ami type of EC2 instance. By default, it is an RHEL Linux distribution"
+  description = "The ami type of EC2 instance. By default, you need to specify it in your variables.tfvars"
   type        = string
-  default     = "ami-0a6351192ce04d50c"
+  default     = ""
 }
 
 variable "instance_type" {
-  description = "Instance type, by default, it is a t3.micro"
+  description = "Instance type. By default, you need to specify it in your variables.tfvars"
   type        = string
-  default     = "t3.micro"
+  default     = ""
 }
 
 variable "key_pair_ssh" {
-  description = "Specify key pair for SSH authentication to EC2 instance"
+  description = "Specify key pair for SSH authentication to EC2 instance. By default, you need to specify it in your variables.tfvars"
   type        = string
-  default     = "keypairssh"
+  default     = ""
 }
 
 variable "ebs_size" {
-  description = "EBS volume size, by default, it is 8 GiB size"
+  description = "EBS volume size. By default, you need to specify it in your variables.tfvars"
   type        = number
-  default     = 8
+  default     = 0
 }
